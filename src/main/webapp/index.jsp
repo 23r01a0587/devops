f0e42eae8a5aaadf20518d5e63a059c807601af1
student <student@host.docker.internal>
2025-12-31 10:53:28 +0530

    new java file

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2><%= "Hello World!" %></h2>
+<h2><%="Check for changes"%></h2>
 </body>
 </html>
---
+++ test.java
@@ -0,0 +1,6 @@
+
+public class test {
+	public static void main(String args[]) {
+		System.out.println("Demo for exp-2");
+	}
+}
